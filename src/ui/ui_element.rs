56afb67c7744cb9ab52673298a2cab1e96201d1e
GatoImorrivel <guilhermevianagui23@gmail.com>
2021-10-31 03:52:09 -0300

    little bug

--- src/ui/ui_element.rs
+++ src/ui/ui_element.rs
@@ -6,9 +6,12 @@ use super::Context;
 use super::Text;
 use super::vector2::Vector2F;
 
+const ANTI_ALIASING: f32 = 4.0;
+
 pub struct UIElement {
     id: String,
     text: Text,
+    font_size: f32,
     position: Vector2F,
 }
 
@@ -16,19 +19,24 @@ impl UIElement {
     pub fn new(id: &str, str: &str, font_size: f32, position: Vector2F) -> Self {
         Self {
             id: id.to_string(),
+            font_size,
             text: Text::new(
-                TextFragment::new(str.to_string()).scale(PxScale { x: (font_size * 4.0), y: (font_size * 4.0) })
+                TextFragment::new(str.to_string()).scale(PxScale { x: (font_size * ANTI_ALIASING), y: (font_size * ANTI_ALIASING) })
             ),
             position
         }
     }
 
     pub fn update_text_static(&mut self, str: &str) {
-        self.text = Text::new(str);
+        self.text = Text::new(
+            TextFragment::new(str.to_string()).scale(PxScale { x: (self.font_size * ANTI_ALIASING), y: (self.font_size * ANTI_ALIASING) })
+        );
     }
 
     pub fn update_text_string(&mut self, str: String) {
-        self.text = Text::new(str);
+        self.text = Text::new(
+            TextFragment::new(str.to_string()).scale(PxScale { x: (self.font_size * ANTI_ALIASING), y: (self.font_size * ANTI_ALIASING) })
+        );
     }
 
     pub fn draw(&self, ctx: &mut Context) {
@@ -44,8 +52,8 @@ impl UIElement {
             )
             .scale(
                 graphics::mint::Point2 {
-                    x: 0.25,
-                    y: 0.25,
+                    x: 1.0 / ANTI_ALIASING,
+                    y: 1.0 / ANTI_ALIASING,
                 }
             ),
         )
